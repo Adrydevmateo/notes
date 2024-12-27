bbf55827ce30293013a952c44e4c151f189ee4ea
AdryDev <adrydevmateo@gmail.com>
2024-12-27 13:57:59 -0400

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { useNotesStore } from "./stores/notes";
+
+const notesStore = useNotesStore();
+
+const addNote = () => {
+    notesStore.addNote({
+        title: "New note",
+        body: "This is the body",
+        category: "technology",
+    });
+};
+</script>
+
+<template>
+    <h1>Home Page</h1>
+    <button type="button" @click="addNote">Add Note</button>
+    <div v-for="note in notesStore.notes" :key="note.id">
+        <p>{{ note.title }}</p>
+    </div>
+</template>
+
+<style scoped></style>
---
+++ src/stores/notes.js
@@ -0,0 +1,37 @@
+import { defineStore } from "pinia";
+import { computed, ref } from "vue";
+
+export const useNotesStore = defineStore("notes", () => {
+  const notes = ref([]);
+
+  const notesByCategory = computed((category) =>
+    notes.value.filter((f) => f.category === category),
+  );
+
+  const addNote = (note) => {
+    const newNote = {
+      id: crypto.randomUUID(),
+      title: note.title,
+      body: note.body,
+      category: note.category,
+    };
+    notes.value.unshift(newNote);
+  };
+
+  const updateNote = (note) => {
+    notes.value = notes.value.map((m) => {
+      if (m.id === note.id) {
+        m.title = note.title;
+        m.body = note.body;
+        m.category = note.category;
+      }
+      return m;
+    });
+  };
+
+  const deleteNote = (id) => {
+    notes.value = notes.value.filter((f) => f.id !== id);
+  };
+
+  return { notes, notesByCategory, addNote, updateNote, deleteNote };
+});
